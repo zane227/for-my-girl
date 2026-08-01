449ea6ae98d7ad7e95a59218111f285038da58b6
zane227 <aungheinkyaw2004@gmail.com>
2026-08-01 16:11:24 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const words = [
+    "Happy",
+    "National",
+    "Girlfriend",
+    "Day",
+    "Mg",
+    "Thae",
+    "tone",
+    "lay" ,
+    "❤️",
+];
+
+const typing = document.getElementById("typing");
+const finalText = document.getElementById("finalText");
+const bgm = document.getElementById("bgm");
+
+let index = 0;
+
+function showWords() {
+
+    if(index < words.length){
+
+        typing.style.opacity = 1;
+typing.style.transform = "scale(1)";
+
+        setTimeout(()=>{
+
+            typing.innerHTML = words[index];
+            typing.style.opacity = 1;
+
+            index++;
+
+            setTimeout(showWords,4000);
+
+        },800);
+
+    }else{
+
+        setTimeout(()=>{
+
+            typing.style.display = "none";
+
+            finalText.style.display = "block";
+
+        },800);
+
+    }
+
+}
+
+showWords();
+
+/* Music */
+bgm.volume = 0.5;
+
+bgm.play().then(()=>{
+
+    showWords();
+
+}).catch(()=>{
+
+    document.body.addEventListener("click",()=>{
+
+        bgm.play();
+        showWords();
+
+    },{once:true});
+
+});
